1cdae33e4bb5b9419f07e8737c7490e42395112a
Sam Rosenbluth <samuel.rosenbluth@gmail.com>
2016-12-13 17:52:33 -0600

    starting making the routes

---
+++ server/routes/comments.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+ const express = require('express');
+ const router = express.Router();
+ var knex = require('../../db/knex.js');
+ var bcrypt = require('bcrypt-as-promised');
+
+
+
+ module.exports = router;
