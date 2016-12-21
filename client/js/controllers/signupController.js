4d1bc9354acbd01b5d4b6aa451c71079ba24761d
Sam Rosenbluth <samuel.rosenbluth@gmail.com>
2016-12-20 19:24:29 -0600

    captures the req.body info correctly

--- client/js/controllers/signupController.js
+++ client/js/controllers/signupController.js
@@ -1,4 +1,10 @@
-app.controller('signupController', ['$scope', function($scope){
+app.controller('signupController', ['$scope', '$http', function($scope, $http){
     $scope.view = {};
     $scope.view.test = 'booyah signup';
+    $scope.user = {};
+    $scope.signUp = function(userobj){
+        $http.post('/api/users', userobj).then(function(response){
+            console.log(response);
+        });
+    };
 }]);
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,6 +6,9 @@
  var knex = require('../../db/knex.js');
  var bcrypt = require('bcrypt-as-promised');
 
+ router.post('/users', function(req, res){
+   res.send(req.body)
+ })
 
 
  module.exports = router;
